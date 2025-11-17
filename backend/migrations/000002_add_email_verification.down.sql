36b177049d0ecc5489b80604a007fdc0fe46defc
Prabalesh P <prabalesh2008@gmail.com>
2025-11-17 18:53:16 +0530

    delete migrations

--- backend/migrations/000002_add_email_verification.down.sql
+++
@@ -1,7 +0,0 @@
-ALTER TABLE users 
-DROP COLUMN email_verification_token,
-DROP COLUMN email_verification_token_expires_at,
-DROP COLUMN email_verification_attempts,
-DROP COLUMN email_verification_last_sent_at;
-
-DROP INDEX IF EXISTS idx_users_email_verification_token;
